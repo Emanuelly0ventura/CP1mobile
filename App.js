d36e491e766f8203001a1cc85798fd1f5c0b1953
manu <emanuelly.ventura.n@gmail.com>
2026-03-18 16:15:19 -0300

    continuação do styles do mobile

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, TextInput, Image, Text, Button, View } from 'react-native';
+import { StyleSheet, TextInput, Image, Text, Button, View, TouchableOpacity } from 'react-native';
 import BancoDados from './components/BancoDados';
+import { CurrentRenderContext } from '@react-navigation/native';
 
 export default function App() {
 const [nome,setNome]=useState("")
@@ -13,9 +14,10 @@ const [dados,setDados]=useState(false)
 
   return (
     <View style={styles.container}>
+      <Text style={styles.titulo}>Bem-vindo ao super genios!</Text>
       <View style={styles.card}>
-        <Text style={styles.titulo}>Bem-vindo ao super genios!</Text>
-        <Text style={styles.titulo}>Insira seus dados</Text>
+        
+        <Text style={styles.texto}>Insira seus dados</Text>
         <TextInput
         
             placeholder='Digite seu nome'
@@ -48,13 +50,19 @@ const [dados,setDados]=useState(false)
             autoCapitalize='words'
             onChangeText={setDescricao}
         />
-      </View>  
-    <Button style={styles.Button}
-    title='Enviar'
-    onPress={()=>setDados(!dados)}
-    />
+      </View> 
+      
+    <View style={styles.Button}>
+      <Button style={styles.Button}
+      title='Enviar'
+      onPress={()=>setDados(!dados)}
+      color="#fff"
+      />
+    </View>
 
-{dados&&<BancoDados nome={nome} curso={curso} disciplina={disciplina} descricao={descricao}/>}
+
+{ dados&&<BancoDados 
+nome={nome} curso={curso} disciplina={disciplina} descricao={descricao}/>}
     </View>
     
   )
@@ -69,17 +77,20 @@ const styles = StyleSheet.create({
 
   },
   titulo:{
-    gap:20,
-    fontSize:20,
+    fontSize:25,
     color:'#fff',
-    alignC: 'center',
-    
+    textAlign: 'center',
+    paddingTop: 70,
+
   },
   card:{
     backgroundColor: '#6b8a9c',
-    padding: 50,
+    paddingTop: 45,
+    paddingBottom: 45,
+    paddingLeft:60,
+    paddingRight:60,
     borderRadius: 10,
-    marginTop: 190,
+    marginTop: 10,
 
   },
   input:{
@@ -91,6 +102,22 @@ const styles = StyleSheet.create({
   },
   Button: {
     padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 12,
+    borderRadius: 10,
+    overflow: 'hidden',
     
+  },
+  
+  texto:{
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  result:{
+    backgroundColor: '#fff'
   }
+
 });
