43844a0c56111f9c096b15be6baf2d63425b1b89
manu <haru.noom0@gmail.com>
2026-05-15 19:54:12 -0300

    fiz o provider do projeto e arrumei o banco, tive problemas na hora de abaixar a dependencias pois usei um comando errado e tive que abaixar tudo de novo, mas deu tudo certo.

--- components/BancoDados.jsx
+++ components/BancoDados.jsx
@@ -1,6 +1,10 @@
 import { Text } from "react-native";
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
 
 export default({nome,curso,disciplina,descricao})=>{
+const { setUsuario } = useContext(UserContext);
+
     return(
         <>
         <Text>Nome:  {nome}</Text>
