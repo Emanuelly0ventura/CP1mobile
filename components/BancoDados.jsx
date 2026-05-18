d4df1203bca62d08432ed533a559a925a57d8799
manu <haru.noom0@gmail.com>
2026-05-18 19:40:32 -0300

    alterrei os dados para encaixar o ViaCep(API externa)

--- components/BancoDados.jsx
+++ components/BancoDados.jsx
@@ -2,14 +2,15 @@ import { Text } from "react-native";
 import { UserContext } from "../context/UserContext";
 import { useContext } from "react";
 
-export default({nome,curso,disciplina,descricao})=>{
+export default({nome,curso,CEP,descricao})=>{
 const { setUsuario } = useContext(UserContext);
 
     return(
         <>
         <Text>Nome:  {nome}</Text>
         <Text>Curso:  {curso}</Text>
-        <Text>Disciplina:  {disciplina}</Text>
+        <Text>CEP:  {CEP}</Text>
+        
         <Text>Descrição:  {descricao}</Text>
 
         </>
